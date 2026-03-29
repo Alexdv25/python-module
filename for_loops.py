7dd21a19ce1076bff06c03393c11e45b3d05856a
Alex.dv <alexdv@gmail.com>
2026-03-29 11:57:09 +0300

    traning loops

--- for_loops.py
+++ for_loops.py
@@ -12,12 +12,29 @@
 # for number in range(0, 10,-1):
 #     print(f"{number} is my friend")
 
-grades = [80, 75, 90, 100, 85]
-amount = len(grades)
-total = 0
+# grades = [80, 75, 90, 100, 85]
+# amount = len(grades)
+# total = 0
 
-for grade in grades:
-    #total = total + grade
-    total += grade
+# for grade in grades:
+#     #total = total + grade
+#     total += grade
     
-print(total/amount)
+# print(total/amount)
+
+# for num in range(1,6):
+#     print(num)
+
+# for num in range(5,0, -1):
+#     print(num)
+
+# for num in range(0,11,2):
+#     print(num)
+    
+# for num in range(0,21,3):
+#     print(num)
+
+
+for i in range(1,6):
+    i = "Hello"
+    print(i)
