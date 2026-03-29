c8ab487469fa33db23c59b295b086a0ced5b88a4
Alex.dv <alexdv@gmail.com>
2026-03-29 10:29:02 +0300

    Add step function

--- for_loops.py
+++ for_loops.py
@@ -1,8 +1,13 @@
-friends = ['Rolf', 'Bob', 'Jen', 'Anne']
+# friends = ['Rolf', 'Bob', 'Jen', 'Anne']
 
-for friend in friends:
-    print(f"{friend} is my friend") 
+# for friend in friends:
+#     print(f"{friend} is my friend") 
     
-#range()
-for friend in range(4):
-    print(f"{friend} is my friend")
+# #range()
+# for number in range(4):
+#     print(f"{number} is my friend")
+    
+    
+#steps()
+for number in range(100, 150, 10):
+    print(f"{number} is my friend")
